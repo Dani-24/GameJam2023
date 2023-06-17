d081c63c3f6cb12e01649d9e73887178656104af
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2023-06-17 21:18:42 +0200

    Palanca
    Amogus

---
+++ GameJam23/Assets/Scripts/Palanca.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Palanca : MonoBehaviour
+{
+    public bool activated;
+
+    [SerializeField]
+    List<string> InteractTags = new List<string>();
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        animator.SetBool("startOn", activated);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (var tag in InteractTags)
+        {
+            if (collision.CompareTag(tag))
+            {
+                activated = !activated;
+                animator.SetTrigger("toggle");
+            }
+        }
+    }
+}
